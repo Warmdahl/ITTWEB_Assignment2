4f63f813ac167dda207855a83d262238e9883fe8
Warmdahl <emilwarmdahl@gmail.com>
2020-11-12 16:25:30 +0100

    tidying up

--- WEB-Angular-Frontend/src/app/app-routing.module.ts
+++ WEB-Angular-Frontend/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule, Routes } from '@angular/router';
-import { AddActivityComponent } from '../app/add-activity/add-activity.component';
-import { AddExerciseComponent } from '../app/add-exercise/add-exercise.component';
-import { AllWorkoutsComponent } from '../app/all-workouts/all-workouts.component';
-import { LoginComponent } from '../app/login/login.component';
-import { WorkoutDetailsComponent } from '../app/workout-details/workout-details.component';
+import { AddActivityComponent } from '../app/Components/add-activity/add-activity.component';
+import { AddExerciseComponent } from '../app/Components/add-exercise/add-exercise.component';
+import { AllWorkoutsComponent } from '../app/Components/all-workouts/all-workouts.component';
+import { LoginComponent } from '../app/Components/login/login.component';
+import { WorkoutDetailsComponent } from '../app/Components/workout-details/workout-details.component';
 
 const routes: Routes = [
   {path: 'addactivity', component: AddActivityComponent},
--- WEB-Angular-Frontend/src/app/app.module.ts
+++ WEB-Angular-Frontend/src/app/app.module.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms'; //NgModels
 import { HttpClientModule } from '@angular/common/http'; //HTTP
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { AllWorkoutsComponent } from './all-workouts/all-workouts.component';
-import { WorkoutDetailsComponent } from './workout-details/workout-details.component';
-import { AddExerciseComponent } from './add-exercise/add-exercise.component';
-import { AddActivityComponent } from './add-activity/add-activity.component';
+import { LoginComponent } from './Components/login/login.component';
+import { AllWorkoutsComponent } from './Components/all-workouts/all-workouts.component';
+import { WorkoutDetailsComponent } from './Components/workout-details/workout-details.component';
+import { AddExerciseComponent } from './Components/add-exercise/add-exercise.component';
+import { AddActivityComponent } from './Components/add-activity/add-activity.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
