d0b1c5c424581f7e05ab25481846f730a2440f71
Warmdahl <emilwarmdahl@gmail.com>
2020-11-13 12:44:08 +0100

    added nodemon

--- Backend_Express/Controllers/usercontroller.js
+++ Backend_Express/Controllers/usercontroller.js
@@ -1,12 +1,7 @@
 var mongoose = require('mongoose')
 const UserList = mongoose.model('User')
 const bcrypt = require('bcrypt');
-var jwt = require('express-jwt');
-/*var auth = jwt({
-    secret: process.env.JWT_SECRET,
-    userProperty: 'payload',
-    algorithms: 'HS256'
-});*/
+
 
 module.exports.AddUserForm = function(req, res){
     res.render('createuser', {title: 'createuser'})
@@ -48,7 +43,7 @@ module.exports.UserLogIn = async function(req, res) {
                 console.log("correct");
                 user.password = "";
                 token = user.generateJWT();
-                res.send(user, token);
+                res.send(token);
                 //res.redirect('//localhost:8080/workouts/workoutlist')
             } else{
                 //password er forkert
